5a07f4f7d1ad2aa1bca91138d0c53590db5e3e71
Mathurin Gagnon <mtgagnon@umich.edu>
2024-04-26 18:49:34 -0400

    added extra things to start

---
+++ resources/component_types/GameManager.lua
@@ -0,0 +1,73 @@
+GameManager = {
+
+	-- TILE CODES --
+	-- 0 : nothing
+	-- 1 : Static box
+	-- 2 : player
+
+	stage1 = {
+		{1, 0, 0, 0, 0, 4, 4, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, -- 20x20
+		{1, 0, 0, 0, 0, 4, 4, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 4, 4, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 3, 3, 1, 1, 1, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 2, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	},
+
+	OnStart = function(self)
+		local controller = 0
+
+		-- Spawn stage
+		for y=1,20 do 
+			for x = 1,20 do
+				local tile_code = self.stage1[y][x]
+				local tile_pos = Vector2(x, y)
+
+				if tile_code == 1 then 
+					local new_tile = Actor.Instantiate("KinematicBox")
+					local new_tile = new_tile:GetComponent("Rigidbody")
+					new_tile.x = tile_pos.x
+					new_tile.y = tile_pos.y
+				elseif tile_code == 2 then
+					Debug.Log("instantiating player")
+
+					local new_player = Actor.Instantiate("player")
+					local new_player_rb = new_player:GetComponent("Rigidbody")
+
+					-- CONTROLLER API
+					-- local new_player_controls = new_player:GetComponent("ControllerControls")
+					-- new_player_controls.controller_id = controller
+					-- controller = controller+1
+
+					new_player_rb.x = tile_pos.x
+					new_player_rb.y = tile_pos.y
+				elseif tile_code == 3 then
+					local new_bol = Actor.Instantiate("Bol")
+					local new_bol_rb = new_bol:GetComponent("Rigidbody")
+					new_bol_rb.x = tile_pos.x
+					new_bol_rb.y = tile_pos.y
+
+				end
+			end
+		end
+	end,
+
+	OnUpdate = function(self)
+		
+	end
+}
+
