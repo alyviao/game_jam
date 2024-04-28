106fbac379642e1fc0e0257b4c3c22a249f6a30e
Mathurin Gagnon <mtgagnon@umich.edu>
2024-04-28 13:31:41 -0400

    updated camera and world

--- resources/component_types/KeyboardControlsP2.lua
+++ resources/component_types/KeyboardControlsP2.lua
@@ -1,6 +1,6 @@
 KeyboardControlsP2 = {
 	speed = 5,
-	jump_power = 400,
+	jump_power = 10,
 	OnStart = function(self)
 		self.rb = self.actor:GetComponent("Rigidbody")
 		self.current_vertical = 0
@@ -18,14 +18,16 @@ KeyboardControlsP2 = {
 			horizontal_input = -1 * self.speed
 			self.sprite_renderer.scale_x = -1
 		end
+
 		local vertical_input = 0
-		if Input.GetKeyDown("w") and self.current_vertical > -.2 then
+		local ground_object = Physics.Raycast(self.rb:GetPosition(), Vector2(0, 1), 0.5)
+		if Input.GetKeyDown("w") and ground_object ~= nil then
 			vertical_input = -1* self.jump_power
+			self.rb:SetVelocity(Vector2(horizontal_input, vertical_input))
 		end
-		self.current_vertical = self.current_vertical * (1 - self.lerp_factor) + vertical_input * self.lerp_factor
 	
 		self.rb:SetRotation(0)
-		self.rb:AddForce(Vector2(0, vertical_input))
+		-- self.rb:AddForce(Vector2(0, vertical_input))
 		self.rb:SetVelocity(Vector2(horizontal_input, self.rb:GetVelocity().y))
 	end
 }
